1ed53a4e070493f67c738dbf557289cbe3934430
Damon0427 <tanzicheng1234@gmail.com>
2025-11-15 08:52:28 +0000

    router class

--- exp1.py
+++ exp1.py
@@ -1,1 +1,27 @@
-print('hello world')
+from mininet.net import Mininet 
+from mininet.node import Node, OVSKernelSwitch, Controller, RemoteController 
+from mininet.cli import CLI 
+from mininet.link import TCLink 
+from mininet.topo import Topo 
+from mininet.log import setLogLevel, info 
+import argparse
+
+
+# allowing the node to act as a router, and forward packets between its interfaces
+class Router( Node ):
+    def config( self, **params ):
+        super( Router, self ).config( **params )
+        # Enable IP forwarding
+        info('enabling IP forwarding on router\n',self)
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( Router, self ).terminate()
+
+
+def ip(subnet,host,prefix=None):
+
+    addr = '10.0.'+str(subnet)+'.' + str(host)
+    if prefix != None: addr = addr + '/' + str(prefix)
+    return addr
