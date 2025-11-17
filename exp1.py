1c6d8bd954addc90d6520afc6c7ae6d9b36e9821
Zicheng Tan <90937647+Damon0427@users.noreply.github.com>
2025-11-16 16:57:57 -0800

    added comments

--- exp1.py
+++ exp1.py
@@ -33,12 +33,14 @@ class Router( Node ):
 class RTopo(Topo):
     def __init__(self):
         super().__init__()
+        # Define Ip addresses and the router.
         h1 = self.addHost('h1', ip=ip(0,1,24), defaultRoute='via '+ip(0,3))  
         h2 = self.addHost('h2', ip=ip(3,2,24), defaultRoute='via '+ip(3,4))  
         h3 = self.addHost('h3', ip=ip(2,2,24), defaultRoute='via '+ip(2,1))  
         r1 = self.addHost('r1', cls=Router, ip=None)
         r2 = self.addHost('r2', cls=Router, ip=None)
 
+        # Define links between nodes
         self.addLink(h1, r1,
             intfName1='h1-eth0', intfName2='r1-eth0',
             params2={'ip': ip(0,3,24)})
@@ -56,12 +58,14 @@ class RTopo(Topo):
             params1={'ip': ip(3,4,24)})
 
 
+
+# Define static routes on the routers
 def staticRouting(r1, r2):
     r1.cmd('ip route add 10.0.2.0/24 via 10.0.1.2 dev r1-eth1')
     r2.cmd('ip route add 10.0.0.0/24 via 10.0.1.1 dev r2-eth0')
     r2.cmd('ip route add 10.0.3.0/24 via 10.0.1.1 dev r2-eth0')
 
-
+# Helper function to generate IP addresses  with subnet, host, and prefix.
 def ip(subnet,host,prefix=None):
     addr = '10.0.'+str(subnet)+'.' + str(host)
     if prefix != None: addr = addr + '/' + str(prefix)
@@ -69,17 +73,23 @@ def ip(subnet,host,prefix=None):
 
 def main():
     setLogLevel('info')
+    # Create the network topology
     topo = RTopo()
     net = Mininet(topo=topo, link=TCLink, autoSetMacs = True)
     net.start()
     r1, r2 = net.get('r1'), net.get('r2')
 
+    # Configure router interfaces with IP addresses
     r1.cmd('ifconfig r1-eth0 ' + ip(0,3,24))
     r1.cmd('ifconfig r1-eth1 ' + ip(1,1,24))
     r1.cmd('ifconfig r1-eth2 ' + ip(3,4,24))
     r2.cmd('ifconfig r2-eth0 ' + ip(1,2,24))
     r2.cmd('ifconfig r2-eth1 ' + ip(2,1,24))
+
+    # Set up static routing for routers
     staticRouting(r1, r2)
+
+    # use to output ping results to result1.txt
     h1, h2, h3 = net['h1'], net['h2'], net['h3']
     with open('result1.txt', 'w') as f:
         f.write('h1 -> h3\n' + h1.cmd('ping -c 1 10.0.2.2') + '\n')
@@ -87,6 +97,7 @@ def main():
         f.write('h3 -> h1\n' + h3.cmd('ping -c 1 10.0.0.1') + '\n')
         f.write('h3 -> h2\n' + h3.cmd('ping -c 1 10.0.3.2') + '\n')
 
+    # Allow user to interact with the network via CLI
     CLI(net) 
     net.stop()
 
--- exp2.py
+++ exp2.py
@@ -12,24 +12,29 @@ from mininet.log import setLogLevel, info
          h2
 
 """
-
 class LinearTopo(Topo):
     def __init__(self):
         super().__init__()
+
+        # Define hosts and switches
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
+
+        # Define links between nodes
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(s1, s2)
         self.addLink(s2, h3)
 
 def main():
-    print("Creating network...")
+    info("Creating network...")
     net = LinearTopo()
     setLogLevel('info')
+
+    # Create the Mininet instance with the defined topology, switches, and controller
     net = Mininet(topo = net, 
                 switch = OVSKernelSwitch, 
                 controller = DefaultController,
@@ -37,7 +42,7 @@ def main():
                 )
     net.start()
     info('Network created.\n')
-    
+
     #sudo ovs-ofctl add-flow s1 "in_port=2,actions=drop"
     #sudo ovs-ofctl add-flow s1 "in_port=1,actions=output:3"
 
